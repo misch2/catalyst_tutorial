5d48bc72ed7bfc4a05737250eee3a2f01d0d82d5
Michal Schwarz <michal.schwarz@gmail.com>
2020-09-22 09:45:06 +0200

    html view

--- MyApp/lib/MyApp/Controller/Books.pm
+++ MyApp/lib/MyApp/Controller/Books.pm
@@ -28,6 +28,30 @@ sub index :Path :Args(0) {
 }
 
 
+=head2 list
+
+Fetch all book objects and pass to books/list.tt2 in stash to be displayed
+
+=cut
+
+sub list :Local {
+    # Retrieve the usual Perl OO '$self' for this object. $c is the Catalyst
+    # 'Context' that's used to 'glue together' the various components
+    # that make up the application
+    my ($self, $c) = @_;
+
+    # Retrieve all of the book records as book model objects and store in the
+    # stash where they can be accessed by the TT template
+    # $c->stash(books => [$c->model('DB::Book')->all]);
+    # But, for now, use this code until we create the model later
+    $c->stash(books => '');
+
+    # Set the TT template to use.  You will almost always want to do this
+    # in your action methods (action methods respond to user input in
+    # your controllers).
+    $c->stash(template => 'books/list.tt2');
+}
+
 
 =encoding utf8
 
